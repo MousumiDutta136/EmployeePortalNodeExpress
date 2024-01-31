53317ff9dce1ec8fbe4b675457f4c48b1d5ea56b
Mousumi <dutta.mousumi136@gmail.com>
2024-01-31 13:07:50 -0500

    added express async handler

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,23 +1,24 @@
+const asyncHandler = require("express-async-handler");
 // get all employees
 //route: GET /api/employees
 //accessibility: public
 
-const getEmployees = async (req,res) => { 
+const getEmployees = asyncHandler(async (req,res) => { 
   res.status(200).json({message:"Get all employees"});
   
-}
+});
 
 //get an employee by id
 //route: GET /api/employees/:id
 //accessibility: public
-const getEmployee = async (req,res) => { 
+const getEmployee = asyncHandler(async (req,res) => { 
   res.status(200).json({message:`get employee for id  ${req.params.id}`});  
-}
+});
 
 // create an employee
 //route: POST /api/employees
 //accessibility: public
-const createEmployee = async (req,res) => { 
+const createEmployee = asyncHandler(async (req,res) => { 
   console.log("The request for create employee" , req.body);
   const {name, email} = req.body;
   if(!name || !email){
@@ -25,21 +26,21 @@ const createEmployee = async (req,res) => {
     throw new Error("All fields are required!");
   }
   res.status(201).json({message:"Create an employee"});  
-};
+});
 
 //update an employee by id
 //route: PUT /api/employees/:id
 //accessibility: public
-const updateEmployee = async (req,res) => { 
+const updateEmployee = asyncHandler(async (req,res) => { 
   res.status(200).json({message:`Update employee for id  ${req.params.id}`});  
-};
+});
 
 //delete an employee by id
 //route: DELETE /api/employees/:id
 //accessibility: public
-const deleteEmployee = async (req,res) => { 
+const deleteEmployee = asyncHandler(async (req,res) => { 
   res.status(200).json({message:`delete employee for id  ${req.params.id}`});  
-}
+});
 
 
 module.exports = {getEmployees, 
